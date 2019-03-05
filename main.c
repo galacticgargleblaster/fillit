4ea90f9cd00f354b4e5536d257b9048dcb545aa1
Nicholas Kirkby <nkirkby@e1z4r6p11.42.us.org>
2019-03-05 11:50:16 -0800

    adding simple colorization

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "lib/libft/libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define CYAN "\033[7;96m"
+#define YELLOW "\033[7;93m"
+#define MAGENTA "\033[7;95m"
+#define BLUE "\033[7;94m"
+#define ORANGE "\033[7;33m"
+#define GREEN "\033[7;32m"
+#define RED "\033[7;31m"
+
+
+int main()
+{
+	ft_putstr(CYAN);
+	ft_putstr("HMM\n");
+	ft_putstr(MAGENTA);
+	ft_putstr("GORGEOUS\n");
+	return (0);
+}
+
