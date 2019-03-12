d400e9e8498ac2d33200073b17c5157f3a612cba
Nicholas Kirkby <nkirkby@e1z4r6p11.42.us.org>
2019-03-12 00:39:44 -0700

    wrote some failing tests for tetromino overlapping

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:39:23 by marvin            #+#    #+#             */
-/*   Updated: 2019/03/08 17:31:48 by marvin           ###   ########.fr       */
+/*   Updated: 2019/03/12 00:17:37 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,81 @@
 #include "fillit.h"
 
 /*
+**	
+*/
+
+#define LEFT_EDGE(tet) (tet->coord.x + tet->x_min)
+#define RIGHT_EDGE(tet) (tet->coord.x + tet->x_max)
+#define TOP_EDGE(tet) (tet->coord.y + tet->y_min)
+#define BOTTOM_EDGE(tet) (tet->coord.y + tet->y_max)
+
+int		intersects_with(t_tetromino *a, t_tetromino *b)
+{
+	unsigned char	x;
+	unsigned char	y;
+
+	if (LEFT_EDGE(b) > RIGHT_EDGE(a) || RIGHT_EDGE(b) < LEFT_EDGE(a) ||
+		BOTTOM_EDGE(b) > TOP_EDGE(a) || TOP_EDGE(b) < BOTTOM_EDGE(a))
+		return (0);
+	else
+	{
+		y = MAX(BOTTOM_EDGE(a), BOTTOM_EDGE(b));
+		while (y < MIN(TOP_EDGE(a), TOP_EDGE(b)))
+		{
+			x = MAX(LEFT_EDGE(a), LEFT_EDGE(b));
+			while (x < MIN(LEFT_EDGE(a), LEFT_EDGE(b)))
+			{
+				if (((*a->shape)[y - a->coord.y][x - a->coord.x] == FILLED) &&
+					((*b->shape)[y - b->coord.y][x - b->coord.x] == FILLED))
+					return (1);
+				x++;
+			}
+			y++;
+		}
+	}
+	return (0);
+}
+
+int		fits_within_board_of_size(t_tetromino *tet, unsigned char sidelength)
+{
+	return (tet->coord.x + tet->x_max < sidelength && tet->coord.x + tet->x_min > 0 &&
+			tet->coord.y + tet->y_max < sidelength && tet->coord.y + tet->y_min > 0);
+}
+
+/*
 **	Check for collisions with a list of already placed tetrominoes, and with
 **	the board boundaries.
 */
 
-int		fits_on_board(t_tetromino *tetromino, t_list* existing,
-						unsigned char sidelength)
-{
-	while (existing)
-	{
-		
-		existing = existing->next;
-	}	
-}
+// int		fits_on_board(t_tetromino *tetromino, t_list* existing,
+// 						unsigned char sidelength)
+// {
+// 	while (existing)
+// 	{
+// 			
+// 		existing = existing->next;
+// 	}	
+// }
 
 #define	INITIAL_BOARD_SIDELENGTH 2
 
-int		naive_solve_inner(t_list *tet_list, t_board board)
-{
-	while (tet_list)
-	{
-		advance_tetromino()
-		if (fits_on_board(board, (t_tetromino *)(tet_list->content))
-			naive_solve_inner(tet_list->next, )
-	}
-}
-
-int		naive_solve(t_list *tet_list)
-{
-	t_board	board;
-	size_t	i;
-
-	i = 0;
-	while (i < MAX_BOARD_SIDELENGTH)
-		ft_bzero(board[i++], MAX_BOARD_SIDELENGTH);
-	naive_solve_inner(tet_list, board);
-}
+// int		naive_solve_inner(t_list *tet_list, t_board board)
+// {
+// 	while (tet_list)
+// 	{
+// 		advance_tetromino()
+// 		if (fits_on_board(board, (t_tetromino *)(tet_list->content))
+// 			naive_solve_inner(tet_list->next, )
+// 	}
+// }
+// 
+// int		naive_solve(t_list *tet_list)
+// {
+// 	t_board	board;
+// 	size_t	i;
+// 
+// 	i = 0;
+// 	while (i < MAX_BOARD_SIDELENGTH)
+// 		ft_bzero(board[i++], MAX_BOARD_SIDELENGTH);
+// 	naive_solve_inner(tet_list, board);
+// }
--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/07 14:39:23 by marvin            #+#    #+#             */
-/*   Updated: 2019/03/11 22:36:31 by marvin           ###   ########.fr       */
+/*   Updated: 2019/03/12 00:39:09 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,26 @@
 #include "../constructors.c"  // Include the .c file to get access to a static function.  Because "translation units" or something
 #define COLORS
 #include "../print.c"
+#include "../solve.c"
 
 #define GREEN_OK() (ft_putstr(GREEN), ft_putstr("OK"), ft_putstr(RESET))
 #define RED_FAIL() (ft_putstr(RED), ft_putstr("FAIL"), ft_putstr(RESET))
+
+#define ASSERT(cond) (\
+	{\
+		int result = cond;\
+		if (!result)\
+		{\
+			ft_putstr(#cond);\
+			ft_putstr(" failed\n");\
+			ft_putstr(__FILE__);\
+			ft_putstr("\n");\
+			ft_putstr(ft_itoa(__LINE__));\
+			ft_putstr("\n");\
+		}\
+		result;\
+	}\
+)\
 
 static int g_err; 
 
@@ -62,6 +79,13 @@ t_shape valid_l_shape = {
 	{'.', '.', '.', '.'},
 };
 
+t_shape valid_j_shape = {
+	{'.', '#', '#', '.'},
+	{'.', '#', '.', '.'},
+	{'.', '#', '.', '.'},
+	{'.', '.', '.', '.'},
+};
+
 int	test_min_max_finding_with_i()
 {
 	t_tetromino tet = 
@@ -70,7 +94,7 @@ int	test_min_max_finding_with_i()
 	};
 
 	init_tet_min_max(&tet);
-	assert(tet.x_min == 0);
+	ASSERT(tet.x_min == 0);
 	assert(tet.x_max == 0);
 	assert(tet.y_min == 0);
 	assert(tet.y_max == 3);
@@ -92,11 +116,41 @@ int	test_min_max_finding_with_o()
 	return (0);
 }
 
+
+int	test_that_non_overlapping_tetrominoes_dont_overlap_basic()
+{
+	t_tetromino *o = new_tetromino(&valid_o_shape);
+	t_tetromino *i = new_tetromino(&valid_i_shape);
+
+	return ASSERT(!intersects_with(o, i));
+}
+
+int	test_that_non_overlapping_tetrominoes_dont_overlap()
+{
+	t_tetromino *o = new_tetromino(&valid_o_shape);
+	t_tetromino *j = new_tetromino(&valid_j_shape);
+
+	return ASSERT(!intersects_with(o, j));
+}
+
+int	test_that_overlapping_tetrominoes_do_overlap()
+{
+	t_tetromino *o = new_tetromino(&valid_o_shape);
+	t_tetromino *j = new_tetromino(&valid_j_shape);
+	j->coord.x = 1;
+
+	return ASSERT(intersects_with(o, j));
+
+}
+
 int	main()
 {
 
 	DO_TEST(test_min_max_finding_with_i);
 	DO_TEST(test_min_max_finding_with_o);
+	DO_TEST(test_that_non_overlapping_tetrominoes_dont_overlap_basic);
+	DO_TEST(test_that_non_overlapping_tetrominoes_dont_overlap);
+	DO_TEST(test_that_overlapping_tetrominoes_do_overlap);
 
 	if (g_err == 0)
 	{
